7159b1d09f84b5164f3426ff3c9cb066f811ed75
mackenza <a.mackenzie@gmail.com>
2017-05-08 21:42:01 -0400

    delete .env

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const graf = require('discord-graf');
+const bot = new graf.Bot({
+    name: 'ESOTrialsBot',
+    version: '1.0.0',
+    about: 'A simple WIP Discord bot for ESO guilds to do signups for groups (e.g. trials)',
+    token: process.env.TOKEN,
+    commandPrefix: '??',
+    clientOptions: {
+        disableEveryone: true
+    }
+})
